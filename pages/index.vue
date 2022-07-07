f9c2e1be32b3a268062aeb5f1710fb3e8c136a44
Renato DelPupo <16341269+renatodelpupo@users.noreply.github.com>
2022-07-06 22:20:52 -0300

    refactor: index file

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="bg-zinc-900 min-h-screen">
+    <h1
+      class="bg-zinc-800 font-semibold px-8 py-4 text-3xl text-center text-slate-100"
+    >
+      Team Sessions
+    </h1>
+
+    <div class="p-8 md:py-16"></div>
+  </div>
+</template>
